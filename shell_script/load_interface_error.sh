5d3e8b11c01ebb74aacdb07c86755fdc83a0611b
Fernando Hernandez <fernandohernandez@MacBook-Air-de-Fernando.local>
2024-11-07 17:31:09 -0600

    Correccion interfaces_error sh

--- shell_script/load_interface_error.sh
+++ shell_script/load_interface_error.sh
@@ -13,15 +13,23 @@ echo "Imprimiendo fecha y hora"
 echo "$FECHA"
 
 ## Activando entorno virtual
-echo "Activando entorno virtual"
-echo "Ruta entorno virtual: "$PY_BIN_DIR
-source $PY_BIN_DIR"activate"
-cd "$SOURCE_CODE_DIR"
+if [[ -f "$PY_BIN_DIR/activate" ]]; then
+    echo "Activando entorno virtual"
+    echo "Ruta entorno virtual: $PY_BIN_DIR"
+    source "$PY_BIN_DIR/activate"
+else
+    echo "Error: No se encuentra el entorno virtual en $PY_BIN_DIR"
+    exit 1
+fi
+
+cd "$SOURCE_CODE_DIR" || { echo "Error: No se pudo acceder al directorio $SOURCE_CODE_DIR"; exit 1; }
 
 ## Ejecutar script
 echo "Ejecutar script  que descarga e inserta información de poleos de error de interfaces a dwh cada 4 minutos"
 echo $SOURCE_CODE_DIR"main_error_interfaces.py"
+
 python3 "$SOURCE_CODE_DIR"main_error_interfaces.py > $SOURCE_CODE_DIR"logs/log_load_python_error_interfaces.log" 2>&1
+
 echo "Terminó el proceso"
 ## python3 main.py Etl-LoadStatus --from_date=$FECHA --to_date=$FECHA  >> $SOURCE_CODE_DIR"logs/out-etl-load-status.log"
 
